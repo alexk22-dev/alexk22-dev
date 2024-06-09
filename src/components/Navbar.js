4ca12d8b9d2ff1ec39612cd1c4c03fbe5a86d094
Alexander Kim <90787108+alexk22-dev@users.noreply.github.com>
2024-06-09 18:39:31 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/alexk22-dev/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
