c6561807439f34a37f15b67400f1a49a1b2878e8
Alexander Kim <90787108+alexk22-dev@users.noreply.github.com>
2024-06-14 10:22:34 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "src/components/Resume/Work Résumé _ Alexander Kim.pdf";
+import pdf from "/Users/alexkim/Downloads/Portfolio-master/src/Assets/Work Résumé _ Alexander Kim.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
